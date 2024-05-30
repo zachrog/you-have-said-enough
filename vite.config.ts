fbcbf4cf34eadf61b48fcaaeaf22b836be905fa7
nathanfronk <n_fronk@apple.com>
2024-05-30 16:53:09 -0700

    kind of working, got a db, made a button, moved files. Next step p2p video

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
+  root: "ui",
 });

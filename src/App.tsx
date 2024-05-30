d55e1743da8d788b167b75ef3de17c959c6046f1
nathanfronk <n_fronk@apple.com>
2024-05-30 15:04:34 -0700

    Made remote video component and p2p streams.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const rTCPeerConnnection = new RTCPeerConnection(servers);
+
   return (
     <>
-      <CameraComponent></CameraComponent>
+      <LocalVideo rTCPeerConnnection={rTCPeerConnnection}></LocalVideo>
+      <RemoteVideo rTCPeerConnnection={rTCPeerConnnection}></RemoteVideo>
     </>
   );
 }
 
-const CameraComponent = () => {
+const LocalVideo = ({rTCPeerConnnection}: {rTCPeerConnnection: RTCPeerConnection}) => {
   const [stream, setStream] = useState<MediaStream | null>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -22,9 +26,13 @@ const CameraComponent = () => {
       setStream(stream);
     };
 
+    
     if (!stream) {
       getUserMedia();
     } else {
+      stream.getTracks().forEach((track) => {
+        rTCPeerConnnection?.addTrack(track, stream)
+      })
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
       }
@@ -41,8 +49,47 @@ const CameraComponent = () => {
 
   return (
     <div>
-      <h1>Camera Component</h1>
-      <video className="h-100 w-64" ref={videoRef} autoPlay playsInline />
+      <h1>Local Video</h1>
+      <video className="h-300 w-900" ref={videoRef} autoPlay playsInline />
+    </div>
+  );
+};
+
+const RemoteVideo = ({rTCPeerConnnection}: {rTCPeerConnnection: RTCPeerConnection}) => {
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (!remoteStream) {
+      setRemoteStream(new MediaStream())
+      // rTCPeerConnnection.ontrack
+    } else {
+      rTCPeerConnnection.ontrack = event => {
+        event.streams[0].getTracks().forEach(track => {
+          remoteStream.addTrack(track)
+        })
+      }
+
+      rTCPeerConnnection.createOffer().then((offerDescription) => {console.log(offerDescription)});
+
+      if (videoRef.current) {
+        videoRef.current.srcObject = remoteStream;
+      }
+    }
+
+    return () => {
+      if (remoteStream) {
+        remoteStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+    };
+  }, [remoteStream]);
+
+  return (
+    <div>
+      <h1>Remote Component</h1>
+      <video className="h-300 w-900" ref={videoRef} autoPlay playsInline />
     </div>
   );
 };

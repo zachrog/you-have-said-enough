e9bcf97d5a6a2c175d8ef89f1fad834dc678b0d8
Zachary Rogers <zrogers>
2024-08-10 15:09:11 -0700

    Add a landing page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,25 @@
-import { RoomPage } from "@/pages/RoomPage";
-import { Button } from "./components/ui/button";
-import { useState } from "react";
+import { RightArrowIcon } from "@/components/icons/icons";
+import { Input } from "@/components/ui/input";
 
 function App() {
-  const [joinedRoom, setJoinedRoom] = useState(false);
   return (
     <>
-      <div className="">
-        {!joinedRoom && (
-          <Button onClick={() => setJoinedRoom(true)}>
-            Join if you D.A.R.E.
-          </Button>
-        )}
-        {joinedRoom && <RoomPage />}
+      <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center justify-center space-y-6">
+          <div className="flex items-center justify-center">
+            <h1 className="text-9xl">Zuumb</h1>
+          </div>
+          <div className="w-full max-w-[600px] rounded-full border-white border-2 antialiased">
+            <form className="flex items-center rounded-full bg-background px-4 py-2 shadow-sm">
+              <Input
+                type="text"
+                placeholder="Join or Create Room"
+                className="flex-1 border-none bg-transparent focus-visible:ring-transparent"
+              />
+              <RightArrowIcon className="h-6 w-6" />
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
--- ui/src/components/icons/icons.tsx
+++ ui/src/components/icons/icons.tsx
@@ -79,3 +79,20 @@ export function SpeakerIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function RightArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+    >
+      <path d="M5 12h14" />
+      <path d="m12 5 7 7-7 7" />
+    </svg>
+  );
+}

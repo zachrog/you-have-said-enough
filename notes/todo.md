749df37d8e579bd108321ba7d4d264ffa9f0443c
Zachary Rogers <zrogers>
2024-12-12 13:54:27 -0800

    Add posthog to capture visitor counts

--- notes/todo.md
+++ notes/todo.md
@@ -7,7 +7,6 @@ Absolutely necessary
 - Stability test with multiple people to decide if there should be a maximum
 
 Nice to have
-- Implement speech as a currency model
 - Implement a measurement tool to track unique visits.
 - Have the Zuumb logo resize on the home page
 
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,13 @@ import { HomePage } from "./pages/HomePage";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RoomPage } from "@/pages/RoomPage.tsx";
+import { PostHogProvider } from "posthog-js/react";
+import { PostHogConfig } from 'posthog-js';
+
+const options: Partial<PostHogConfig> = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+  autocapture: false,
+};
 
 const router = createBrowserRouter([
   {
@@ -17,6 +24,11 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <RouterProvider router={router} />
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+    options={options}
+  >
+    <RouterProvider router={router} />
+  </PostHogProvider>
   // </React.StrictMode>,
 );
